07b13a87bfe11fd1680925be4fc009b5a69a3459
Kevin Kong <kevinyckong@gmail.com>
2014-08-18 19:37:07 -0400

    testing

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='django',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
+    author='test',
+    author_email='test@test.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django<=1.4'],
 )
